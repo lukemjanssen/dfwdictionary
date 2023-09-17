c4429d4b43bf3634d63926ed4dcaff0fe04c12c5
lukemjanssen <jukelanssen64@gmail.com>
2023-09-17 16:44:08 -0500

    updated frontend

--- dfwdictionaryfrontend/src/components/Word.js
+++ dfwdictionaryfrontend/src/components/Word.js
@@ -26,7 +26,7 @@ export default function Word() {
     e.preventDefault();
     const dfwWord = { word, definition };
     console.log(dfwWord);
-    fetch("http://localhost:8080/word/add", {
+    fetch("http://containers-us-west-55.railway.app:8080/word/add", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(dfwWord),
@@ -38,7 +38,7 @@ export default function Word() {
   };
 
   const handleClickSortAscending = (e) => {
-    fetch("http://localhost:8080/word/sortByLengthAsc")
+    fetch("http://containers-us-west-55.railway.app:8080/word/sortByLengthAsc")
       .then((res) => res.json())
       .then((result) => {
         setWords(result);
@@ -46,7 +46,7 @@ export default function Word() {
   };
 
   const handleClickSortDescending = (e) => {
-    fetch("http://localhost:8080/word/sortByLengthDesc")
+    fetch("http://containers-us-west-55.railway.app:8080/word/sortByLengthDesc")
       .then((res) => res.json())
       .then((result) => {
         setWords(result);
@@ -57,7 +57,7 @@ export default function Word() {
     //get the word connected to the delete button
     const id = e.currentTarget.parentNode.id;
     console.log(id);
-    fetch("http://localhost:8080/word/delete/" + id, {
+    fetch("http://containers-us-west-55.railway.app:8080/word/delete/" + id, {
       method: "DELETE",
     }).then(() => {
       console.log("Word deleted");
@@ -67,7 +67,7 @@ export default function Word() {
   };
 
   useEffect(() => {
-    fetch("http://localhost:8080/word/getAll")
+    fetch("http://containers-us-west-55.railway.app:8080/word/getAll")
       .then((res) => res.json())
       .then((result) => {
         setWords(result);
@@ -169,7 +169,7 @@ export default function Word() {
                 });
             } else {
               changeSortButtonText("Sort By: Default");
-              fetch("http://localhost:8080/word/getAll")
+              fetch("http://containers-us-west-55.railway.app:8080/word/getAll")
                 .then((res) => res.json())
                 .then((result) => {
                   setWords(result);
